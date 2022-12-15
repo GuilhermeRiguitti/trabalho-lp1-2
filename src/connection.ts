0f9d2433d5aef7b262b1078fc654b53311f395be
Guilherme Riguitti <71913845+GuilhermeRiguitti@users.noreply.github.com>
2022-12-15 20:42:32 -0300

    Update connection.ts

--- src/connection.ts
+++ src/connection.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-export const connection = knex({  // Estabelece conexão com o banco
+export const connection = knex({  
   client: "mysql",
   connection: {
       host: process.env.DB_HOST,
